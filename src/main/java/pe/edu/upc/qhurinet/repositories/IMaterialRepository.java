5dc2de2851cedbd363bb986064630d509565d55d
unknown <u202224171@upc.edu.pe>
2026-05-05 09:58:27 -0500

    Clean up Victoria query changes

--- src/main/java/pe/edu/upc/qhurinet/repositories/IMaterialRepository.java
+++ src/main/java/pe/edu/upc/qhurinet/repositories/IMaterialRepository.java
@@ -2,11 +2,9 @@ package pe.edu.upc.qhurinet.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import pe.edu.upc.qhurinet.entities.Material;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface IMaterialRepository extends JpaRepository<Material, Integer> {
     @Query(value = "\n" +
@@ -18,17 +16,4 @@ public interface IMaterialRepository extends JpaRepository<Material, Integer> {
             " LIMIT 5",
             nativeQuery = true)
     public List<Object[]> top5MaterialesMasReciclados();
-
-    @Query(value = """
-        SELECT u.id, u.nombre, COALESCE(AVG(c.puntuacion), 0) AS puntuacion_promedio,
-               COUNT(r.id) AS total_recolecciones
-        FROM usuario u
-        INNER JOIN recoleccion r ON u.id = r.id_recolector
-        INNER JOIN calificacion c ON r.id = c.id_recoleccion
-        WHERE u.id = :idRecolector
-        AND r.estado = 'completada'
-        GROUP BY u.id, u.nombre
-        """, nativeQuery = true)
-    List<Object[]> promedioCalificacionRecolector(@Param("idRecolector") UUID idRecolector);
 }
-
